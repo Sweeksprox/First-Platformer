60e969afad1e09bf98e384cff88436956a4a9c97
Sweeks <john.suihkonen@gmail.com>
2016-03-14 11:49:06 -0500

    comments

--- Player.cs
+++ Player.cs
@@ -31,15 +31,18 @@ public class Player : MonoBehaviour {
 
 	void Update () {
 		if (controller.collisions.above || controller.collisions.below) {
+			//if collisions above or below stop moving up/down
 			velocity.y = 0;
 		}
 
+		//get unity's built in horizontal axis controls, left right arrow standard
 		float horizontalInput = Input.GetAxisRaw ("Horizontal");
 
 		if (Input.GetKeyDown(KeyCode.Space) && controller.collisions.below) {
 			velocity.y = jumpVel;
 		}
 
+		//set target velocity for smoothing/damping
 		float targetVelX = horizontalInput * moveSpeed;
 		
 		//smooth out the horizontal acceleration/decelleration, less jerky and jumpy movement
