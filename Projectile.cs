af5ec54430a908be8bd3a76a41812ade5cbd94f7
Sweeks <john.suihkonen@gmail.com>
2016-03-14 13:02:27 -0500

    Update Projectile.cs

--- Projectile.cs
+++ Projectile.cs
@@ -1,13 +1,36 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
+[RequireComponent (typeof(Controller))]
 public class Projectile : MonoBehaviour {
 
+	public float speed;
+
+	public float gravity;
+	public float friction;
+
+	public float bounceCoefficient;
+
+	Vector2 velocity;
+
+	Controller controller;
+
 	void Start () {
-	
+		controller = GetComponent<Controller> ();
+
+		velocity.y = 0;
+		velocity.x = speed;
 	}
 
 	void Update () {
-	
+		if (controller.collisions.below || controller.collisions.above) {
+			velocity.y *= Mathf.Sign(velocity.y) * controller.collisions.hitNormal.y * bounceCoefficient;
+			velocity.x += Mathf.Sign(velocity.x) * friction;
+		}
+		if (controller.collisions.left || controller.collisions.right) {
+			velocity.x *= Mathf.Sign(velocity.x) * controller.collisions.hitNormal.x * bounceCoefficient;
+		}
+		velocity.y += gravity * Time.deltaTime;
+		controller.Move (velocity * Time.deltaTime);
 	}
 }
